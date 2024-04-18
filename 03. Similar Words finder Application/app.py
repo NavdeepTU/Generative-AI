4a7eb34d52f87b844fe54d049a6504a67ade4424
Navdeep Sharma <navdeepsharmatu@gmail.com>
2024-04-18 14:51:53 +0530

    added click button functionality

--- 03. Similar Words finder Application/app.py
+++ 03. Similar Words finder Application/app.py
@@ -20,3 +20,18 @@ loader = CSVLoader(file_path="myData.csv", csv_args={
 
 data = loader.load()
 print(data)
+
+db = FAISS.from_documents(data, embeddings)
+
+def get_text():
+    input_text = st.text_input("You: ", key = input)
+    return input_text
+
+user_input = get_text()
+submit = st.button('Find Similar Things')
+
+if submit:
+    docs = db.similarity_search(user_input)
+    st.subheader("Top Matches:")
+    st.text(docs[0].page_content)
+    st.text(docs[1].page_content)
