9772a79cb96344ecab5955158056246db9578da8
Navdeep Sharma <navdeepsharmatu@gmail.com>
2024-12-28 12:44:47 +0530

    prompt template and response generation function code added

--- course/11. End To End Q&A Chatbot/01. Open-ai chatbot/app.py
+++ course/11. End To End Q&A Chatbot/01. Open-ai chatbot/app.py
@@ -12,3 +12,20 @@ load_dotenv()
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "Simple Q&A Chatbot With OPENAI"
+
+# prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+
+# response generation function
+def generate_response(question, api_key, engine, temperature, max_tokens):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model=engine)
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+    answer = chain.invoke({'question':question})
+    return answer
