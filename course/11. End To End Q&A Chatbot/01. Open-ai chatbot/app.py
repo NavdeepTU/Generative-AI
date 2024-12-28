eacbd1c6d52332f1e611c0fb11ed3e4f43ac79b1
Navdeep Sharma <navdeepsharmatu@gmail.com>
2024-12-28 12:30:37 +0530

    add langchain tracking

---
+++ course/11. End To End Q&A Chatbot/01. Open-ai chatbot/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Langsmith Tracking
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Simple Q&A Chatbot With OPENAI"
