17ef4daf5ef0d70ae292427627addf7ff0d96f47
Navdeep Sharma <navdeepsharmatu@gmail.com>
2024-12-29 09:34:20 +0530

    add web interface code using streamlit

--- course/11. End To End Q&A Chatbot/02. Ollama chatbot/app.py
+++ course/11. End To End Q&A Chatbot/02. Ollama chatbot/app.py
@@ -3,7 +3,7 @@ import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
 import os
 from dotenv import load_dotenv
 
@@ -21,7 +21,7 @@ prompt = ChatPromptTemplate.from_messages([
 
 def generate_response(question, llm, temperature, max_tokens):
     # initialize the ollama model
-    llm = ollama(model=llm)
+    llm = Ollama(model=llm)
     # create an output parser to handle the model's response
     output_parser = StrOutputParser()
     # create a chain: prompt -> llm -> parser
@@ -30,3 +30,19 @@ def generate_response(question, llm, temperature, max_tokens):
     answer = chain.invoke({"question":question})
     return answer
 
+# application interface
+# title of the app
+st.title("Enhanced Q&A Chatbot With OpenAI")
+# sidebar controls
+llm = st.sidebar.selectbox("Select Open Source model", ["llama3.2"])
+temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+# main interface
+st.write("Go ahead and ask any question")
+user_input = st.text_input("You:")
+
+if user_input:
+    response = generate_response(user_input, llm, temperature, max_tokens)
+    st.write(response)
+else:
+    st.write("Please provide the user input")
