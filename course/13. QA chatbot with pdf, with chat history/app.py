b8a4b88353626e79fba82264b54b1aa13a136208
Navdeep Sharma <navdeepsharmatu@gmail.com>
2024-12-31 15:49:37 +0530

    add huggingface embeddings, streamlit and Groq setup code

---
+++ course/13. QA chatbot with pdf, with chat history/app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_groq import ChatGroq
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+# set up streamlit
+st.title("Conversational RAG With PDF uploads and chat history")
+st.write("Upload PDF's and chat with their content")
+
+# input the Groq API key
+api_key = st.text_input("Enter your Groq API key:", type="password")
+
