c4e57f9f5d145424334863497d11e63309c3ee7e
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-01-15 12:22:49 +0530

    add reasoning tool code and an agent to combine all tools to generate the response

--- course work/12. text_to_math_problem_solver/app.py
+++ course work/12. text_to_math_problem_solver/app.py
@@ -50,3 +50,43 @@ prompt_template = PromptTemplate(
 
 # combine all the tools into chain
 chain = LLMChain(llm=llm, prompt=prompt_template)
+reasoning_tool = Tool(
+    name="Reasoning tool",
+    func=chain.run,
+    description="A tool for answering logic based and reasoning questions."
+)
+
+# initialize the agents
+
+assistant_agent = initialize_agent(
+    tools=[wikipedia_tool,calculator,reasoning_tool],
+    llm=llm,
+    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    verbose=False,
+    handling_parsing_errors=True
+)
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role":"assistant", "content":"Hi, I am a Math chatbot who can answer all your math questions"}
+    ]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+# Let us start the interaction
+question = st.text_area("Enter your question:", "I have 5 bananas and 7 grapes. I eat 2 bananas and give away 3 grapes. Then I buy a dozen apples and 2 packs of blueberries. Each pack of blueberries contains 25 berries. How many total pieces of fruit do I have at the end?")
+
+if st.button("find my answer"):
+    if question:
+        with st.spinner("Generate response.."):
+            st.session_state.messages.append({"role":"user", "content":question})
+            st.chat_message("user").write(question)
+
+            st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+            response = assistant_agent.run(st.session_state.messages, callbacks=[st_cb])
+            st.session_state.messages.append({"role":"assistant", "content":response})
+            st.write("### Reaponse:")
+            st.success(response)
+    else:
+        st.warning("Please enter the question")
