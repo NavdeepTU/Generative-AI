25878ef2170aeb1ffced5068c06fee369e0ee948
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-02-03 07:07:05 +0530

    set session for sagemaker

---
+++ course work/16. GenAI with AWS/06. deployment_on_aws_sagemaker.py
@@ -0,0 +1,10 @@
+import sagemaker
+import boto3
+
+sess = sagemaker.Session()
+# sagemaker session bucket -> used for uploading data, models and logs
+# sagemaker will automatically create this bucket if it not exists
+sagemaker_session_bucket = None
+if sagemaker_session_bucket is None and sess is not None:
+    # set to default bucket if a bucket name is not given
+    sagemaker_session_bucket = sess.default_bucket()
