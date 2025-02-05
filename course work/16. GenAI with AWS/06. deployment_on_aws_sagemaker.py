84c56c6bb35b759b3d00b3e042d855dba3dc5dfa
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-02-05 13:47:06 +0530

    creating and deploying hugging face model to sagemaker inference

--- course work/16. GenAI with AWS/06. deployment_on_aws_sagemaker.py
+++ course work/16. GenAI with AWS/06. deployment_on_aws_sagemaker.py
@@ -1,5 +1,6 @@
 import sagemaker
 import boto3
+from sagemaker.huggingface.model import HuggingFaceModel
 
 sess = sagemaker.Session()
 # sagemaker session bucket -> used for uploading data, models and logs
@@ -20,3 +21,34 @@ session = sagemaker.Session(default_bucket=sagemaker_session_bucket)
 
 print(f"sagemaker role arn:{role}")
 print(f"sagemaker session region:{session.boto_region_name}")
+
+hub = {
+    "HF_MODEL_ID":"distilbert-base-uncased-distilled-squad", # model_id from hf.co/models
+    "HF_TASK":"question-answering" # NLP task you want to use for predictions
+}
+
+# create hugging face model class
+huggingface_model = HuggingFaceModel(
+    env=hub, # configuration for loading model from hub
+    role=role, # IAM role with permissions to create an endpoint
+    transformers_version="4.26", # Transformers version used
+    pytorch_version="1.13", # pytorch version used
+    py_version="py39" # python version used
+)
+
+# deploy model to SageMaker Inference
+predictor = huggingface_model.deploy(
+    initial_instance_count=1,
+    instance_type="ml.m5.xlarge"
+)
+
+# example request: you always need to define "inputs"
+data = {
+    "inputs": {
+        "question": "What is used for inference?",
+        "context": "My name is Navdeep and I live in Noida. This model is used with sagemaker for inference."
+    }
+}
+
+# request
+predictor.predict(data)
