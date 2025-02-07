ecf3a4e1227c15c8fa8fd9367f0b7569efaba523
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-02-07 11:10:41 +0530

    loading libraries and llm using ChatNVIDIA along with embedding model

---
+++ course work/17. end_to_end_gen_ai_app_using_nvidia_nim/02. rag_using_nvidia.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import os
+from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# load the nvidia api key
+os.environ["NVIDIA_API_KEY"] = os.getenv("NVIDIA_API_KEY")
+llm = ChatNVIDIA(model="meta/llama3-70b-instruct")
+
+def vector_embedding():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings = NVIDIAEmbeddings()
+        st.session_state.loader = PyPDFDirectoryLoader("./us_census")
