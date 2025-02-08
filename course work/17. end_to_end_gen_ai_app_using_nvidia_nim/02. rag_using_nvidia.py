5bcc80b1a92ecd942da4a370ebb9e0b3085e6909
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-02-08 07:57:50 +0530

    add ui interface to create document embeddings and generate response for user query

--- course work/17. end_to_end_gen_ai_app_using_nvidia_nim/02. rag_using_nvidia.py
+++ course work/17. end_to_end_gen_ai_app_using_nvidia_nim/02. rag_using_nvidia.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import time
 from langchain_nvidia_ai_endpoints import NVIDIAEmbeddings, ChatNVIDIA
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -14,9 +15,47 @@ load_dotenv()
 
 # load the nvidia api key
 os.environ["NVIDIA_API_KEY"] = os.getenv("NVIDIA_API_KEY")
-llm = ChatNVIDIA(model="meta/llama3-70b-instruct")
+llm = ChatNVIDIA(model="meta/llama3-70b-instruct") # NVIDIA NIM Inferencing
 
 def vector_embedding():
     if "vectors" not in st.session_state:
         st.session_state.embeddings = NVIDIAEmbeddings()
         st.session_state.loader = PyPDFDirectoryLoader("./us_census")
+        st.session_state.docs = st.session_state.loader.load()
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=50) # chunk creation
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:30]) # splitting
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+st.title("Nvidia NIM Demo")
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    <context>
+    Questions:{input}
+    """
+)
+
+prompt1 = st.text_input("Enter Your Question From Documents")
+
+if st.button("Document Embedding"):
+    vector_embedding()
+    st.write("FAISS Vector Store DB is ready using NvidiaEmbeddings")
+
+if prompt1:
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = st.session_state.vectors.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    start = time.process_time()
+    response = retrieval_chain.invoke({"input":prompt1})
+    print("Response time : ", time.process_time() - start)
+    st.write(response["answer"])
+
+    # with a streamlit expander
+    with st.expander("Document Similarity Search"):
+        # find the relevant chunks
+        for i, doc in enumerate(response["context"]):
+            st.write(doc.page_content)
+            st.write("-----------------------------------")
