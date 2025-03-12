07158790b4abe7526ecdbb44a0e859f81ab23e82
Navdeep Sharma <navdeepsharmatu@gmail.com>
2025-03-12 08:22:00 +0530

    structured output using TypedDict, Pydantic and json_schema in langchain

---
+++ langchain_indepth/03_langchain_structured_output/03_pydantic_demo.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class Student(BaseModel):
+
+    name: str = "navdeep" # seting default value as "navdeep"
+    age: Optional[int] = None # optinal and default value None
+    email: EmailStr # data type for builtin validation of email in pydantic
+    cgpa: float = Field(gt=0, lt=10, default=5, description="A decimal value representing the cgpa of the student") # applying constraint and default value, same as Annotated in TypedDict
+    # can also add regualr expressions for phone number 
+
+
+new_student = {"name": "nitish",  # cannot be integer
+               "age": "32",  # data type coercing (if possible)
+               "email": "abc@gmail.com",
+               "cgpa": 8,
+               } 
+
+student = Student(**new_student)
+
+print(student) # pydantic object
+
+student_dict = dict(student) # converting to dict
+
+print(student_dict["age"])
+
+student_json = student.model_dump_json() # converting to json
+
